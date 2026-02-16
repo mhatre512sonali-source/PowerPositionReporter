47b92083643d1b2ba018c900f8ba889ad8f50c9f
Sonali Mhatre <sonalimhatre@Vipuls-MacBook-Air.local>
2026-02-16 23:31:54 +0100

    Refactor Program file

--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -1,70 +1,61 @@
+using System;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using Axpo;
+using NodaTime;
+using Microsoft.Extensions.Options;
+using Serilog;
 using WorkerService;
 using WorkerService.Configuration;
-using WorkerService.Services;
 using WorkerService.Interfaces;
-using Microsoft.Extensions.Hosting;
-using Serilog;
+using WorkerService.Services;
 
 try
 {
     var host = Host.CreateDefaultBuilder(args)
-        .UseSerilog()
+        .UseSerilog((context, services, loggerConfiguration) =>
+        {
+            var loggingDirectory = context.Configuration.GetValue<string>("PowerPositionSettings:LoggingDirectory") ?? "./logs";
+            Directory.CreateDirectory(loggingDirectory);
+            var logFilePath = Path.Combine(loggingDirectory, "PowerPosition.log");
+
+            loggerConfiguration
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    logFilePath,
+                    rollingInterval: RollingInterval.Day,
+                    fileSizeLimitBytes: 50 * 1024 * 1024,
+                    retainedFileCountLimit: 10,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}");
+        })
         .ConfigureServices((context, services) =>
         {
-            var settings = context.Configuration.GetSection("PowerPositionSettings")
-                        .Get<PowerPositionSettings>() ?? new PowerPositionSettings();
+            services.Configure<PowerPositionSettings>(context.Configuration.GetSection("PowerPositionSettings"));
 
-            services.Configure<PowerPositionSettings>(options =>
-            {
-                options.IntervalMinutes = settings.IntervalMinutes;
-                options.OutputFolder = settings.OutputFolder;
-                options.TimeZoneId = settings.TimeZoneId;
-                options.LoggingDirectory = settings.LoggingDirectory;
-            });
+            services.AddSingleton(sp => sp.GetRequiredService<IOptions<PowerPositionSettings>>().Value);
 
-            services.AddSingleton(settings);
             services.AddScoped<IPowerService, PowerService>();
             services.AddScoped<IPowerTradeProcessor, PowerTradeProcessor>();
             services.AddScoped<ICsvExportService, CsvExportService>();
             services.AddHostedService<Worker>();
-
-
-            //Configure logging
-            var loggingDirectory = settings.LoggingDirectory ?? "./logs";
-            Directory.CreateDirectory(loggingDirectory);
-
-            var logFilePath = Path.Combine(loggingDirectory, "PowerPosition.log");
-
-            Directory.CreateDirectory("logs");
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .WriteTo.Console()
-                .WriteTo.File(
-                    "logs/PowerPosition.log",
-                    rollingInterval: RollingInterval.Hour,
-                    outputTemplate:
-                    "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {SourceContext} {Message}{NewLine}{Exception}")
-                .CreateLogger();
-
-            
-            using var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder.ClearProviders();           
-                builder.AddSerilog();               // Add Serilog as provider
-            });
-            var logger = loggerFactory.CreateLogger<ILogger<Program>>();
-            logger.LogInformation($"Starting Power Position Service - Interval: {settings.IntervalMinutes}min, Output: {settings.OutputFolder}, Logs: {settings.LoggingDirectory}");
-
         })
         .Build();
+
+    var logger = host.Services.GetRequiredService<ILogger<Program>>();
+    var settings = host.Services.GetRequiredService<PowerPositionSettings>();
+    logger.LogInformation("Starting Power Position Service - Interval: {Interval}min, Output: {Output}, Logs: {Logs}", settings.IntervalMinutes, settings.OutputFolder, settings.LoggingDirectory);
 
     host.Run();
 }
 catch (Exception ex)
 {
     Log.Fatal(ex, "Application terminated unexpectedly");
+    throw;
 }
 finally
 {
